1034913e74b7f178604847ce695c1d9432935b32
Khalid Alharbi <kalharbi@users.noreply.github.com>
2023-02-22 13:50:57 +0300

    refactor getGradeLetter

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -7,31 +7,31 @@ public class Calculator {
     }
 
     public String getGradeLetter(int grade) {
-        if(grade < 0|| grade >100){
+        if(grade < 0 || grade >100){
             throw new IllegalArgumentException("Grades can not be below 0 and above 100.");
         }
-        else if (grade >=0 && grade < 60){
+        else if (grade < 60){
             return "F";
         }
-        else if (grade >=60 && grade <65) {
+        else if (grade <65) {
             return "D";
         }
-        else if (grade >=65 && grade <70) {
+        else if (grade <70) {
             return "D+";
         }
-        else if (grade >=70 && grade <75) {
+        else if (grade <75) {
             return "C";
         }
-        else if (grade >=75 && grade <80) {
+        else if (grade <80) {
             return "C+";
         }
-        else if (grade >=80 && grade <85) {
+        else if (grade <85) {
             return "B";
         }
-        else if (grade >=85 && grade <90) {
+        else if (grade <90) {
             return "B+";
         }
-        else if (grade >=90 && grade <95) {
+        else if (grade <95) {
             return "A";
         }
         return "A+";
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -16,20 +16,12 @@ public class CalculatorTest
     @Test
     void shouldReturnGradeLetters(){
         assertEquals("F", calc.getGradeLetter(0));
-        assertEquals("F", calc.getGradeLetter(10));
-        assertEquals("D", calc.getGradeLetter(60));
         assertEquals("D", calc.getGradeLetter(61));
-        assertEquals("D+", calc.getGradeLetter(65));
         assertEquals("D+", calc.getGradeLetter(66));
-        assertEquals("C", calc.getGradeLetter(70));
         assertEquals("C", calc.getGradeLetter(71));
-        assertEquals("C+", calc.getGradeLetter(75));
         assertEquals("C+", calc.getGradeLetter(76));
         assertEquals("B", calc.getGradeLetter(80));
-        assertEquals("B", calc.getGradeLetter(84));
-        assertEquals("B+", calc.getGradeLetter(85));
         assertEquals("B+", calc.getGradeLetter(88));
-        assertEquals("A", calc.getGradeLetter(90));
         assertEquals("A", calc.getGradeLetter(92));
         assertEquals("A+", calc.getGradeLetter(97));
     }
